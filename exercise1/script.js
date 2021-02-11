a971780878b6551b2348a1636841f65ef6ffebba
Uddam <cheara01@luther.edu>
2021-02-11 12:00:35 -0800

    Exercise_1

--- exercise1/script.js
+++ exercise1/script.js
@@ -8,16 +8,33 @@ function greet() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     let greetElement = document.querySelector("h1");
-    greetElement.innerHTML = `Hello ${urlParams.get('name')}`;
+    greetElement.innerHTML = `Hello ${urlParams.get('name')||'Student'}`;
 }
 
 function isPrime(n) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let numberInput = params.get('n')
+        for (var n = 2; n < numberInput; n++) {
+            if ( number % n === 0) {
+                return false;
+            }
+        }
 }
 
 function printPrimeNumber() {
 }
 
 function getNPrimes(n) {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    let numberInput = params.get('n')
+    var arr = [2];
+        for (var n = 3; n < numberInput; n+=2) {
+            if (isPrime(n)) {
+                arr.push(n);
+            }
+        }
 }
 
 function printNPrimes() {
