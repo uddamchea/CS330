cb62d4d8abd6f4e672321732a100e551cd073a22
Uddam <cheara01@luther.edu>
2021-02-19 20:07:33 -0800

    5th_commit

--- projects/calculator/js/calculator.js
+++ projects/calculator/js/calculator.js
@@ -7,16 +7,31 @@ var screen;
 
 
 function enterDigit(digit) {
+    let inputDigit = `${digit}`
+    if(screen.innerHTML == "0") {
+        document.querySelector("#result").innerHTML = inputDigit ;
+    }
+    else{
+        screen.innerHTML= screen.innerHTML + inputDigit
+    }
 }
 
 function clear_screen() {
-    document.getElementById("display").innerHTML = "";
+    document.querySelector("#result").innerHTML = `${"0"}`
 }
 
 function eval_expr() {
+    let operators = (screen.innerHTML.match(/\+/g) || []).length;
+    if(operators>1){
+        screen.innerHTML="ERROR";
+    }
+    else{
+        screen.innerHTML=`${eval(String(screen.innerHTML))}`
+    }
 }
 
 function enterOp(operation) {
+    document.querySelector("#result").append(operation); 
 }
 
 window.onload = function () {
