545b525550114ff72f110a642c113f39e929e973
Uddam <cheara01@luther.edu>
2021-02-26 20:53:39 -0800

    New_commit

--- exercises/todo_list/js/script.js
+++ exercises/todo_list/js/script.js
@@ -9,31 +9,51 @@ function addTask() {
     let vals = [];
     let rowcolids = ["title", "assignedTo", "priority", "dueDate"];
 
+    //Implement
     if (!document.querySelector("#newTask").checkValidity()) {
         let warning = document.createElement("p");
         warning.setAttribute("class", "alert alert-warning");
-        warning.innerText = "Please enter all values";
+        warning.innerText = "Please enter all values!";
         document.querySelector("body").appendChild(warning);
         return;
     }
 
-    //Implement
+    let title = document.querySelector("#title").value;
+    vals.push(title);
+    let team = document.querySelector("#assignedTo").selectedOptions[0].value;
+    vals.push(team);
+    let priority = document.querySelector("#priority").selectedOptions[0].value;
+    vals.push(priority);
+    let dueDate = document.querySelector("#dueDate").value;
+    vals.push(dueDate);
 
     addRow(vals, document.getElementById("taskList"));
 }
 
 function addRow(valueList, parent) {
     let row = document.createElement("tr");
-    let td = document.createElement("td");
     let cb = document.createElement("input");
-
-    // Implement
+    cb.setAttribute("type", "checkbox");
+    cb.setAttribute("id", "eliminate");
+    cb.onclick = removeRow();
+    let checker = document.createElement("td");
+    checker.appendChild(cb);
+    row.appendChild(checker);
+    for (let value of valueList){
+        let td = document.createElement("td");
+        td.document.createElement("td");
+        td.innerHTML = value;
+        row.appendChild(td);
+    }
 
     parent.appendChild(row);
 }
 
 function removeRow() {
     // https://stackoverflow.com/questions/26512386/remove-current-row-tr-when-checkbox-is-checked
+    $('#btnEliminar').on('click', function () {
+        $("#tablaNorma input[type='checkbox']:checked").closest("tr").remove();
+    });
 }
 
 function populateSelect(selectId, sList) {
