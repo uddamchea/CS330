a2cbabab2ffe20e0fb94857e6100577eef8edf0d
Uddam <cheara01@luther.edu>
2021-03-31 16:20:52 -0700

    1st commit

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -5,7 +5,7 @@ import random
 from typing import List
 
 import pyjokes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, redirect, url_for
 
 app = Flask(__name__)
 
@@ -13,12 +13,14 @@ app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
     """Render the template with form"""
+    return render_template("base.html", name="CS330")
     raise NotImplementedError
 
 
 @app.route("/", methods=["POST"])
 def index_jokes():
     """Render the template with jokes"""
+    return render_template("jokes.html")
     raise NotImplementedError
 
 
@@ -26,4 +28,6 @@ def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
     """Return a list of jokes"""
+    list_of_jokes = pyjokes.get_jokes(language="eng", category="chuck")
+    return list_of_jokes
     raise NotImplementedError
