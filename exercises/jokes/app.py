4595ea6e9f87992bf3ea19b3f5c16b94a4b589d7
Uddam <cheara01@luther.edu>
2021-04-01 20:00:05 -0700

    Almost Done

--- exercises/jokes/app.py
+++ exercises/jokes/app.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 """Flask application to use `pyjokes`"""
 
@@ -5,25 +6,48 @@ import random
 from typing import List
 
 import pyjokes
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, url_for, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["GET"])
 def index():
-    return render_template("base.html", name="CS330")
+    language_of_jokes = request.cookies.get("language_of_jokes")
+    type_of_jokes = request.cookies.get("type_of_jokes")
+    number_of_jokes = request.cookies.get("number_of_jokes")
+
+    if language_of_jokes and type_of_jokes and number_of_jokes:
+        return render_template("base.html", list_of_jokes = send_joke(language_of_jokes, type_of_jokes, number_of_jokes))
+    else:
+        return render_template("base.html")
+
+    raise NotImplementedError
 
 
 @app.route("/", methods=["POST"])
 def index_jokes():
-    return render_template("jokes.html")
+    fetch = make_response(redirect(url_for("index"), code=303))
+    fetch.set_cookie("language_of_jokes", request.form.get("language"))
+    fetch.set_cookie("type_of_jokes", request.form.get("category"))
+    fetch.set_cookie("number_of_jokes", request.form.get("number"))
+    return fetch
+
+    raise NotImplementedError
 
 
 def send_joke(
     language: str = "en", category: str = "all", number: int = 1
 ) -> List[str]:
-    jokes = pyjokes.get_jokes(language='en', category= 'neutral')
 
-    for i in range(10):
-        print(i+1,".",jokes[i])
+    if language=="es" and category=="chuck":
+        return(["No jokes about Chuck Norris in Spanish"])
+
+    get_jokes = pyjokes.get_jokes(language=language, category=category)
+    jokeList = []
+
+    for index in range(int(number)):
+        jokeList.append(get_jokes[index])
+    return jokeList
+
+    raise NotImplementedError
