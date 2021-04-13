ef9e26b248eda7499b448ca7bff0059510fc980a
Uddam <cheara01@luther.edu>
2021-04-13 11:39:39 -0700

    5th commit

--- projects/jokes_api/client/script.js
+++ projects/jokes_api/client/script.js
@@ -3,17 +3,32 @@
 /* jshint browser: true */
 'use strict'
 
-const BASE_URL = "http://localhost:5000/api/v1"
+const BASE_URL = "http://localhost:5000/api/v1/jokes"
 
-async function requestData(typeOfData) {
-    return fetch(`${BASE_URL}/${typeOfData}`)
+async function get_jokes() {
+    var category = document.getElementById(selCat).value;
+    var language = document.getElementById(selLang).value;
+    var number = document.getElementById(selNum).value;
+    var id = document.getElementById().value;
+
+    if (id){
+    var jokes = await fetch(`${BASE_URL}/${category}/${language}/${number}`)
     .then(response => response.json())
-    .then(json => printData(json[typeOfData]))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error))}
+
+    else{
+    var jokes = await fetch(`${BASE_URL}/${category}/${language}/${number}/${id}`)
+    .then(response => response.json())
+    .catch(error => console.log(error))}
+    
+    let row = document.querySelector("#jokes")
+    for (let index in jokes){
+        let td = document.createElement("table");
+        td.innerHTML = jokes[index];
+        row.appendChild(td);
+    }
 }
 
-function printData(data) {
-    let responseDiv = document.querySelector("#response");
-    responseDiv.innerHTML = data;
-}
-
+    async function print_jokes() {
+        pass
+    }
--- projects/jokes_api/server/app.py
+++ projects/jokes_api/server/app.py
@@ -5,35 +5,41 @@ Faker API
 
 import json, pyjokes
 from flask import Flask, Response, jsonify
-from flask_cors import CORS, cross_origin
-from faker import Faker
 
 app = Flask(__name__)
 
 @app.route("/api/v1/jokes/<category>/<language>/<int:number>")
-def get_joke(language, category, number):
-    if language== "es" and category== "chuck" and number == 1:
+def get_jokes(category, language, number):
+    newDict = {}
+    if language== "es" and category== "chuck":
+        return "No Jokes about Chuck Norris in Spanish!"
+    else:
+        allJokes = pyjokes.get_jokes(language=language, category=category)
+        jokeDict = {}
+
+        for m in range(len(allJokes)):
+            jokeDict[m] = allJokes[m]
+        for n in range(number):
+            newDict[n] = jokeDict[n]
+
+        jsonify(newDict).headers["Access-Control-Allow-Origin"] = "*"
+        jsonify(newDict).headers["Content-Type"] = "application/json"
+        return jsonify(newDict)
+
+@app.route("/api/v1/jokes/<category>/<language>/<int:number>/<int:id>")
+def get_specific(category, language, number, id):
+    if language== "es" and category== "chuck":
         return "No Kidding!"
 
     else:
+        allJokes = pyjokes.get_jokes(language=language, category=category)
+        jokeDict = {}
 
-        jokes = pyjokes.get_jokes(language=language, category=category)
-        dic = {}
+        for i in range(len(allJokes)):
+            jokeDict[i] = allJokes[i]
 
-        for i in range(len(jokes)):
-            dic[i] = jokes[i]
-
-        jokes = jsonify(dic)
-        jokes.headers["Access-Control-Allow-Origin"] = "*"
-        jokes.headers["Content-Type"] = "application/json"
-        return jokes
-        #return jsonify(jokes = pyjokes.get_jokes(language=language, category=category))
-
-@app.route("/api/v1/address")
-@cross_origin()
-def get_address():
-    return jsonify(address=fake.address())
-
+        allJokes = {id:jokeDict[id]}
+        return jsonify(allJokes)
 
 if __name__ == "__main__":number
 app.run("0.0.0.0")
