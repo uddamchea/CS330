2dae0f61054690c82bfdc20e0ebc3924d453f044
Uddam <cheara01@luther.edu>
2021-05-06 01:04:21 -0700

    2nd commit

--- projects/final_project/app.py
+++ projects/final_project/app.py
@@ -1,8 +1,78 @@
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, jsonify, request, render_template, url_for, redirect
 import json
 import random
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import sqlite3 as sql
+import sqlite3
 
 app = Flask(__name__)
+
+conn = sqlite3.connect('database.db')
+print ("Opened database successfully");
+
+# conn.execute('CREATE TABLE ACTION (name TEXT, year TEXT, genre TEXT, producer TEXT)')
+# print ("Table created successfully");
+
+# conn.execute('CREATE TABLE ADVENTURE (name TEXT, year TEXT, genre TEXT, producer TEXT)')
+# print ("Table created successfully");
+
+# conn.execute('CREATE TABLE ROMCOM (name TEXT, year TEXT, genre TEXT, producer TEXT)')
+# print ("Table created successfully");
+# conn.close()
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return render_template("login.html")
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+    return render_template("home.html")
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template("login.html")
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return render_template("login.html")
+
+@app.route("/guest", methods=['GET', 'POST'])
+def guest():
+    return render_template("guest.html")
+
+@app.route("/server", methods=['GET', 'POST'])
+def server():
+    return render_template("server.html")
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        try:
+            name = request.form['name']
+            year = request.form['year']
+            genre = request.form['genre']
+            producer = request.form['producer']
+                
+            with sql.connect("database.db") as con:
+                cur = con.cursor()
+                cur.execute("INSERT INTO ACTION (name,year,genre,producer) VALUES (?,?,?,?)",(name,year,genre,producer) )
+                    
+            con.commit()
+            msg = "Record successfully added"
+        except:
+            con.rollback()
+            msg = "error in insert operation"
+            
+        finally:
+            return render_template("result.html",msg = msg)
+            con.close()
+    return render_template("add.html")
+
+@app.route("/result", methods=['GET', 'POST'])
+def result():
+    return render_template("result.html")
 
 @app.route("/api/v1/action")
 def get_action():
@@ -26,7 +96,7 @@ def get_action():
         "Black Widow",
         "The Suicide Squad",
         "Spider-Man: No Way Home",
-        "Tenet"
+        "Batman v Superman"
     ]
 
     res = Response(json.dumps({"name": movie_action}))
